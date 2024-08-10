d0dab92aae3f146e43e8eb9b99c03a3567d85e8f
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-10 14:25:56 -0300

    Add: Get and Create Tasks

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   FlatList,
+  Keyboard,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -10,20 +11,72 @@ import {
 } from 'react-native';
 import Login from './src/components/Login';
 import TaskItem from './src/components/TaskItem';
+import firebase from './src/services/firebaseConnection';
 
 const App = () => {
   const [user, setUser] = useState(null);
   const [newTask, setNewTask] = useState('');
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+
+    const getUser = () => {
+      if(!user){
+        return;
+      }
+
+      firebase.database().ref('tasks').child(user).once('value', (snapshot) => {
+        setTasks([]);
+
+        snapshot?.forEach((childItem) => {
+          let data = {
+            key: childItem.key,
+            task: childItem.val().task
+          }
+          setTasks(oldTasks => [...oldTasks, data])
+        })
+      });
+    }
+
+    getUser();
+
+  },[user])
+
+
 
   if (!user) {
     return <Login changeUser={user => setUser(user)} />;
+  }  
+  
+  const handleAdd = () => {
+    if(newTask === ''){
+      return;
+    }
+    let tasks = firebase.database().ref('tasks').child(user);
+    let key = tasks.push().key;
+
+    tasks.child(key).set({
+      task: newTask
+    })
+    .then(() => {
+      const data = {
+        key: key,
+        task: newTask
+      }
+      setTasks(oldTasks => [...oldTasks, data])
+    })
+
+    Keyboard.dismiss();
+    setNewTask('');
   }
 
-  let list = [
-    {key: 1, name: 'buy coke'},
-    {key: 2, name: 'run'},
-    {key: 3, name: 'yes'}
-  ]
+  const handleDelete = (key) => {
+    console.log(key)
+  }
+
+  const handleEdit = (data) => {
+    console.log('Item Clicked', data)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -34,15 +87,15 @@ const App = () => {
           value={newTask}
           onChangeText={text => setNewTask(text)}
         />
-        <TouchableOpacity style={styles.addBtnArea}>
+        <TouchableOpacity style={styles.addBtnArea} onPress={handleAdd}>
           <Text style={styles.addBtnText}>+</Text>
         </TouchableOpacity>
       </View>
 
       <FlatList 
-      data={list}
+      data={tasks}
       keyExtractor={(item) => item.key}
-      renderItem={({item}) => <TaskItem data={item}/>}
+      renderItem={({item}) => <TaskItem data={item} deleteItem={handleDelete} editItem={handleEdit}/>}
       />
     </SafeAreaView>
   );
--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -1,19 +1,38 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
-const TaskItem = ({data}) => {
+const TaskItem = ({data, deleteItem, editItem}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={{marginRight: 10}} onPress={() => deleteItem(data.key)}>
+        <Feather name="trash" color="#fff" size={20} />
+      </TouchableOpacity>
 
-    return(
-        <View style={styles.container}>
-            <Text>{data.name}</Text>
-        </View>
-    )
+      <View style={{paddingRight: 10}}>
+      <TouchableWithoutFeedback onPress={() => editItem(data)}>
+        <Text style={{color: '#fff', paddingRight: 10}}>{data.task}</Text>
+      </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#121212',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 4,
+  },
+});
 
 export default TaskItem;
